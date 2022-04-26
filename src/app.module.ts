4be03e5979784b3f7d6cd638c9410aa6a37ec1af
Richard Venite <richard.miguez@gmail.com>
2022-04-26 19:52:23 -0300

    feat: #4 students module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,7 +14,8 @@ import { Lesson } from './lesson/lesson.entity';
       synchronize: true,
       useUnifiedTopology: true,
       entities:[
-        Lesson
+        Lesson,
+        Student
       ]
     }),
     GraphQLModule.forRoot({
@@ -20,6 +23,7 @@ import { Lesson } from './lesson/lesson.entity';
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
